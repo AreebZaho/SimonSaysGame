b714b47dec831349999802db41f18ef106bd4424
AreebZaho <117965711+AreebZaho@users.noreply.github.com>
2023-08-08 06:27:20 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-let h3 = document.querySelector('h3');
+let h3 = document.getElementById('titleh3');
 let ansSeq = [];
 let level = 0;
 let start = false;
+let score = document.getElementById('score');
+let highScore = document.getElementById('highscore');
+let highScoreNum = 0;
 
-//game starts
-function game() {
-    if (!start) {
-        start = true;
-        levelUp();
-    }
-}//starts by pressing any key / mid 'start' button
+//game start/reset
+//start = true only by pressing any key / mid 'start' button
 document.addEventListener('keydown', game);
 document.getElementById('mid').addEventListener('click', game);
+function gameReset() {
+    score.innerText = 'Score: 0';
+    start = false;
+    h3.innerText = 'Press the center or any key to start the game!';
+}
+function game() {
+    if (start) {//if start already made true (achieved by page-reload/gameReset) then don't do this again
+        return;
+    }
+    start = true;
+    level = 0;
+    levelUp();
+}
 
 function randomBtn() {//generates random btn as in html id and returns id string
     const btns = ['one', 'two', 'three', 'four'];
@@ -32,15 +43,31 @@ function bgFlash() {//on incorrect button clicked
     setTimeout(() => {
         document.querySelector('body').classList.remove('bgFlash');
         document.getElementById('incorrect').style.display = 'none';
-    }, 100);
+    }, 200);
 }
+function userClick(btn) {//to flash the button user clicks for 1ms
+    btn.classList.add('userClick');
+    setTimeout(() => {
+        btn.classList.remove('userClick');
+    }, 75);
+}
+
 let idx = 0;//to track the buttons clicked by user to match real ansSeq
 function levelUp() {
     ansSeq.splice(0, ansSeq.length);//new sequence of answers will be made
     level++;//new level
     h3.innerText = `Level ${level}`;
+    if (level != 1) {//flash a LEVEL UP! sign when its not the first level
+        document.getElementById('levelUp').style.display = 'block';
+        setTimeout(() => {
+            document.getElementById('levelUp').style.display = 'none';
+        }, 250);
+        highScoreNum = Math.max(highScoreNum, level);
+        score.innerText = `Score: ${level-1}`;
+        highScore.innerText = `High Score: ${highScoreNum-1}`;
+    }
     let i = 0;
-    let totalShines = setInterval(() => {//as many shines as the number of level at gaps of 200ms
+    let totalShines = setInterval(() => {//shines = levels at gaps of 100ms as individual btn shine for 100ms also
         let btnShined = btnShine();
         ansSeq.push(btnShined);
         i++;
@@ -49,18 +76,27 @@ function levelUp() {
     idx = 0;//match button id strings by user with the ones in array beginning now as all buttons flashed
 }
 
-//user actions
+//user actions governing everything
 let allBtns = document.querySelectorAll('.btn');
-allBtns.forEach(btn => {//applies on aall 4 buttons
+allBtns.forEach(btn => {//applies on all 4 buttons
     btn.addEventListener('click', function btnClick() {//whenever clicked, 
-        if (ansSeq[idx++] !== this.id) {//from idx = 0, if anywhere the btn id string & ansSeq don't match
-            bgFlash();//trigger flash
-            level = 0;//make level 0 again 
-            setTimeout(() => {//call levelUp which starts level from 1 agiain after 500ms of bgFlash()
-                levelUp();
-            }, 500);
-        } 
-        if (idx == level) levelUp();//if all correct btns clicked, in end idx == level so update level
+        if (start) {//start button functionalities only when any-key/start is pressed and start = true
+            if (ansSeq[idx] !== this.id) {//from idx = 0, if anywhere the btn id string & ansSeq don't match
+                bgFlash();//trigger incorrect flash
+                setTimeout(() => {//incorrect flashes and gameReset called after 400ms 
+                    gameReset();
+                }, 400);
+            } 
+            else {//if the sequence is correct then flash button and update index
+                userClick(this);//trigger flash for correct button in sequence pressed by the user
+                idx++;
+                if (idx == level) {//arr index start = 0, hits level after ++ -> (0 -> level-1 = level number done)
+                    setTimeout(() => {
+                        levelUp();//if all correct btns clicked, in end idx == level so update level after 500ms
+                    }, 400);
+                }
+            }
+        }
     });
 });
 
